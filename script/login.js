557a3a78748e6520cdbfb2d19921e7fe57f2307f
remon918 <mdremonhossen7778@gmail.com>
2026-02-28 23:26:19 +0600

    fifth

--- script/login.js
+++ script/login.js
@@ -15,7 +15,7 @@ document.getElementById('login-btn').addEventListener('click',function(){
         // 3-1: true --> alert= homepage
         alert('Login Successful');
 
-        window.location.assign('./home')
+        window.location.assign('./home.html')
 
     }
     else{
